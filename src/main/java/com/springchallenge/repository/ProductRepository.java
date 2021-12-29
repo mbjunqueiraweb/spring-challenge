e407713a7ed9e32d4a117409f0611661523b9f91
mbjunqueiraweb <mauro.junqueira@mercadolivre.com>
2021-12-29 10:45:28 -0300

    implementando repository

--- src/main/java/com/springchallenge/repository/ProductRepository.java
+++ src/main/java/com/springchallenge/repository/ProductRepository.java
@@ -1,8 +1,39 @@
 package com.springchallenge.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.springchallenge.entity.Product;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class ProductRepository extends RuntimeException{
+public class ProductRepository {
+    public List<Product> products = new ArrayList<Product>();
+    private ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    private final String PATH = "products.json";
 
+    public List<Product> getProducts() throws IOException{
+        File file = new File(PATH);
+        FileInputStream is = new FileInputStream(file);
+        products = Arrays.asList(objectMapper.readValue(is, Product[].class));
+        return products;
+    }
 
+    public Product getProductsById(Long id) throws IOException {
+
+        Product product = getProducts().stream().filter(p -> p.getProductId() == id).findFirst().orElse(new Product());
+
+        return product;
+    }
+
+    public void newProduct(Product product) throws IOException {
+        products = getProducts();
+        product.setProductId((long) products.size()+1);
+        products.add(product);
+        objectMapper.writeValue(new File(PATH), products);
+    }
 }
