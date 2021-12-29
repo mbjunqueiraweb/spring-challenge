aa0557d5ffdb141b0144046ba8a534d27b28d938
mbjunqueiraweb <mauro.junqueira@mercadolivre.com>
2021-12-29 09:45:17 -0300

    adicionando biblioteca jacson

--- src/main/java/com/springchallenge/repository/ProductRepository.java
+++ src/main/java/com/springchallenge/repository/ProductRepository.java
@@ -2,16 +2,7 @@ package com.springchallenge.repository;
 
 import java.util.List;
 
-public class ProductRepository {
+public class ProductRepository extends RuntimeException{
 
-//    public List<Produto> getProducts(Long id) {
-//        List<Produto> product = null;
-//        product.stream().findAny().equals(id);
-//        return product;
-//
-//    }
-//
-//    public List<Procuct> getProducts(Long id) {
-//
-//    }
+
 }
