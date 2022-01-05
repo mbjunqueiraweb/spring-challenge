3522c6d1868ebf3ea58e84ca1aa51625f007dc9c
mbjunqueiraweb <mauro.junqueira@mercadolivre.com>
2022-01-05 10:12:07 -0300

    initial-comit

--- src/main/java/com/springchallenge/controller/ProductController.java
+++ src/main/java/com/springchallenge/controller/ProductController.java
@@ -22,9 +22,23 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<List<ProductDTO>> create(@RequestBody List<Product> produtos){
+<<<<<<< HEAD
         List<ProductDTO> res =   ProductDTO.convertToDTO(produtos);
         return ResponseEntity.ok().body(res);
     }
 
+=======
+        productService.newProduct(produtos);
+        List<ProductDTO> res = ProductDTO.convertToDTO(produtos);
+        return ResponseEntity.ok().body(res);
+    }
+
+    @PostMapping("/purchaseRequest")
+    public ResponseEntity<Ticket> purchaseRequest(@RequestBody List<Product> produtos){
+        Ticket t = productService.newPurchase(produtos);
+        return ResponseEntity.ok(t);
+
+    }
+>>>>>>> b36745207a16dd59403637f27cf049b55f50ea06
 
 }
--- src/main/java/com/springchallenge/service/ProductService.java
+++ src/main/java/com/springchallenge/service/ProductService.java
@@ -1,12 +1,18 @@
 package com.springchallenge.service;
 
 import com.springchallenge.entity.Product;
+<<<<<<< HEAD
+=======
+import com.springchallenge.entity.Ticket;
+import com.springchallenge.exceptions.RepositoryExceptions;
+>>>>>>> b36745207a16dd59403637f27cf049b55f50ea06
 import com.springchallenge.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +38,41 @@ public class ProductService {
     }catch (){
         throw new RuntimeException("usuario deve ser maior de idade");
     }
+
+    public List<Product> listProducts(Product query, int orderBy) {
+        // TODO: Tratar as exceções no RepositoryException
+        // TODO: ordenar
+        try {
+            List<Product> products = productRepository.getProducts();
+            List<Product> filteredProducts = products.stream()
+                    .filter(p -> resolveQuery(p, query)).collect(Collectors.toList());
+            return filteredProducts;
+        } catch (IOException e) {
+            throw new RuntimeException("mm");
+        }
+    }
+
+    public Ticket newPurchase (List<Product> purchase){
+        try {
+            BigDecimal total = new BigDecimal(0);
+            Ticket ticket = new Ticket();
+            List<Product> list = new ArrayList<Product>();
+            for (Product prod :purchase) {
+                Product p = productRepository.getProductsById(prod.getProductId());
+                p.setQuantity(prod.getQuantity());
+                BigDecimal value = new BigDecimal(String.valueOf(p.getPrice().multiply(new BigDecimal(p.getQuantity()))));
+                total = total.add(value);
+                list.add(p);
+            }
+
+            ticket.setArticles(list);
+            ticket.setTotal(total);
+
+            return ticket;
+
+        }catch (IOException e) {
+            throw new RuntimeException("Produto nao encontrado:");
+
         }
         return products;
     }
